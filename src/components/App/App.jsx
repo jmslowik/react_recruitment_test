fb0a6b1437bc3b51c4371e69ce4860eeb52e0dee
Jakub Slowik <jslowik154@gmail.com>
2020-12-21 23:11:46 +0100

    encapsulated ProductsList container

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllAction } from '@/store/products/saga';
-import {
-  setAndVerifyProductQuantityAction,
-  setAndVerifyProductQuantityDebounceAction,
-} from '@/store/basket/saga';
-import { productsListSelector } from '@/store/products/selectors';
-import { basketSelector } from '@/store/basket/selectors';
-import ProductsList from '@/components/ProductsList';
+import React from 'react';
+import ProductsList from '@/containers/ProductsList';
 
-const App = () => {
-  const dispatch = useDispatch();
-  const products = useSelector(productsListSelector);
-  const basket = useSelector(basketSelector);
-
-  const onChange = ({ pid, newValue, min }) => {
-    dispatch(setAndVerifyProductQuantityDebounceAction({ pid, quantity: newValue, min }));
-  };
-
-  useEffect(() => {
-    dispatch(getAllAction());
-  }, [dispatch]);
-
-  useEffect(() => {
-    products.forEach(({ pid, min }) => {
-      dispatch(setAndVerifyProductQuantityAction({ pid, quantity: min, min }));
-    });
-  }, [products, dispatch]);
-
-  return (
-    <ProductsList
-      list={products}
-      quantities={basket}
-      onChange={onChange}
-    />
-  );
-};
+const App = () => (
+  <ProductsList />
+);
 
 export default App;
